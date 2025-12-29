782bfd4a65b9e3184c1dde56c19d413133221792
michael faith <michaelfaith@users.noreply.github.com>
2025-12-29 14:47:36 -0600

    adjust lint config

--- eslint.config.js
+++ eslint.config.js
@@ -80,11 +80,17 @@ export default defineConfig(
 		files: ["**/*.{yml,yaml}"],
 		rules: {
 			"yml/file-extension": ["error", { extension: "yml" }],
-			"yml/sort-keys": [
+			"yml/sort-sequence-values": [
 				"error",
 				{ order: { type: "asc" }, pathPattern: "^.*$" },
 			],
-			"yml/sort-sequence-values": [
+		},
+	},
+	{
+		files: ["pnpm-workspace.yaml"],
+		rules: {
+			"yml/file-extension": "off",
+			"yml/sort-keys": [
 				"error",
 				{ order: { type: "asc" }, pathPattern: "^.*$" },
 			],
